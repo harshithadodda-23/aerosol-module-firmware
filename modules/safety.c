def46b2428d2842f6c1cbd7455d418b84336a3b8
harshithadodda-23 <harshithadod123@gmail.com>
2026-04-23 13:50:52 -0400

    Update safety.c

--- modules/safety.c
+++ modules/safety.c
@@ -1,1 +1,21 @@
+#include "safety.h"
+#include "../mcc_generated_files/pin_manager.h"
 
+bool Safety_CheckOverTemp(float chamberTemp, float heaterTemp)
+{
+    if (chamberTemp >= 120.0f)
+        return true;
+
+    if (heaterTemp >= 120.0f)
+        return true;
+
+    return false;
+}
+
+bool Safety_CheckWater(void)
+{
+    if (WaterPresent_GetValue() == 0)
+        return true;
+
+    return false;
+}
