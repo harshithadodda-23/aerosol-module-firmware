e2b2ee982513f6540ad603073f82f3a39c8e3fac
harshithadodda-23 <harshithadod123@gmail.com>
2026-04-23 13:39:23 -0400

    Update actuators.c

--- Drivers/actuators.c
+++ Drivers/actuators.c
@@ -1,1 +1,35 @@
+#include "actuators.h"
+#include "../mcc_generated_files/pin_manager.h"
 
+//================ WATER PUMP =================//
+void Actuator_WaterPump_On(void)
+{
+    BlowerMotorLgEnable_SetHigh();
+}
+
+void Actuator_WaterPump_Off(void)
+{
+    BlowerMotorLgEnable_SetLow();
+}
+
+//================ DOSING VALVE =================//
+void Actuator_DosingValve_On(void)
+{
+    VentValveInOpen_SetHigh();
+}
+
+void Actuator_DosingValve_Off(void)
+{
+    VentValveInOpen_SetLow();
+}
+
+//================ VENT =================//
+void Actuator_Vent_On(void)
+{
+    VentValveInClosed_SetHigh();
+}
+
+void Actuator_Vent_Off(void)
+{
+    VentValveInClosed_SetLow();
+}
