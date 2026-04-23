57d70bfe9b16a1483d59f10d7b713adbf8ebc0e1
harshithadodda-23 <harshithadod123@gmail.com>
2026-04-23 13:41:46 -0400

    Update app.c

--- app/app.c
+++ app/app.c
@@ -1,1 +1,22 @@
+#include <stdint.h>
 
+volatile uint32_t g_msTicks = 0;   // ? DEFINE HERE
+
+#include "xc.h"
+
+#include "app.h"
+#include "state_machine.h"
+#include "temperature.h"
+#include "level_sensor.h"
+
+void App_Init(void)
+{
+    Temp_Init();
+    SM_Init();
+}
+
+void App_Run(void)
+{
+    SM_Run();
+    Level_Init();
+}
