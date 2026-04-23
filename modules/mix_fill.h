30d8bcae232d542dcaf792f01661e83c71bca894
harshithadodda-23 <harshithadod123@gmail.com>
2026-04-23 13:44:12 -0400

    Update mix_fill.h

--- modules/mix_fill.h
+++ modules/mix_fill.h
@@ -1,1 +1,33 @@
 
+#ifndef MIX_FILL_H
+#define MIX_FILL_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+//==================== STATUS ====================//
+typedef enum
+{
+    MIX_IDLE = 0,
+    MIX_RUNNING,
+    MIX_FILLED,
+    MIX_MAINTAINING,
+    MIX_ABORT,
+    MIX_DONE
+
+} mix_status_t;
+
+//==================== API ====================//
+void Mix_Init(void);
+void Mix_Run(void);
+
+// Control inputs (to be driven by CAN later)
+void Mix_SetStart(uint8_t val);
+void Mix_SetPreheatRequest(uint8_t val);
+
+// Status outputs
+mix_status_t Mix_GetStatus(void);
+
+#endif
+
+
