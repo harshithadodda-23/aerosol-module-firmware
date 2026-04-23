ea8e3f6f90605ab0b740c8bdbb9129fa8a601d5a
harshithadodda-23 <harshithadod123@gmail.com>
2026-04-23 13:50:09 -0400

    Update temperature.h

--- modules/temperature.h
+++ modules/temperature.h
@@ -1,1 +1,13 @@
+#include <xc.h> // include processor files - each processor file is guarded.  
+#ifndef TEMPERATURE_H
+#define TEMPERATURE_H
+
+#include <xc.h>
+#include <stdint.h>
+
+void Temp_Init(void);
+float Temp_GetChamber(void);
+float Temp_GetHeater(void);
+
+#endif   // TEMPERATURE_H
 
