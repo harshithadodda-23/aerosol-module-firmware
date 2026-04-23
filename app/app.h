08d066c44aa943968d16b09ff716b11c1fdb8547
harshithadodda-23 <harshithadod123@gmail.com>
2026-04-23 13:42:24 -0400

    Update app.h

--- app/app.h
+++ app/app.h
@@ -1,1 +1,27 @@
+#ifndef XC_HEADER_TEMPLATE_H
+#define	XC_HEADER_TEMPLATE_H
+#include <stdint.h>
 
+extern volatile uint32_t g_msTicks;   // ? DECLARE
+#include <xc.h> // include processor files - each processor file is guarded.  
+extern volatile uint32_t g_msTicks;
+#ifndef APP_H
+#define APP_H
+
+void App_Init(void);
+void App_Run(void);
+
+#endif
+
+#ifdef	__cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+    // TODO If C++ is being used, regular C code needs function names to have C 
+    // linkage so the functions can be used by the c code. 
+
+#ifdef	__cplusplus
+}
+#endif /* __cplusplus */
+
+#endif	/* XC_HEADER_TEMPLATE_H */
