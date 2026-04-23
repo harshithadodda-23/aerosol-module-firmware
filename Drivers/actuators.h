fddc19c240c23d255fc7904806014f52e11f203d
harshithadodda-23 <harshithadod123@gmail.com>
2026-04-23 13:39:59 -0400

    Update actuators.h

--- Drivers/actuators.h
+++ Drivers/actuators.h
@@ -1,1 +1,13 @@
+#ifndef ACTUATORS_H
+#define ACTUATORS_H
 
+void Actuator_WaterPump_On(void);
+void Actuator_WaterPump_Off(void);
+
+void Actuator_DosingValve_On(void);
+void Actuator_DosingValve_Off(void);
+
+void Actuator_Vent_On(void);
+void Actuator_Vent_Off(void);
+
+#endif
