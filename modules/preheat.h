510f8477e3e2dbb87f709e765e16ec4071b6cb7f
harshithadodda-23 <harshithadod123@gmail.com>
2026-04-24 11:39:57 -0400

    Update preheat.h

--- modules/preheat.h
+++ modules/preheat.h
@@ -1,1 +1,22 @@
+#ifndef PREHEAT_H
+#define PREHEAT_H
 
+#include <stdint.h>
+
+//================ STATUS =================//
+typedef enum
+{
+    PREHEAT_IDLE = 0,
+    PREHEAT_RUNNING,
+    PREHEAT_DONE,
+    PREHEAT_FAULT
+
+} preheat_status_t;
+
+//================ API =================//
+void Preheat_Init(void);
+void Preheat_Start(uint8_t val);
+void Preheat_Run(void);
+preheat_status_t Preheat_GetStatus(void);
+
+#endif
